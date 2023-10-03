a6ce2f031d448bfc2f4e45c3cb0031a78cd78d61
Ramazan TÜFEKÇİ <ramazan.tufekci@outlook.com>
2023-10-03 14:55:06 +0300

    Create BrandController.java

---
+++ PatikaStore/controller/BrandController.java
@@ -0,0 +1,59 @@
+package patikadevstore.controller;
+
+import patikadevstore.model.Brand;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BrandController {
+    private static List<Brand> brands = new ArrayList<>();
+    private static int lastBrandId;
+
+    public BrandController() {
+        createDefaultObjects();
+    }
+
+    private void createDefaultObjects(){
+        String[] brandNames = {"Samsung", "Lenovo", "Apple", "Huawei", "Casper", "Asus", "HP", "Xiaomi", "Monster"};
+
+        for (String name : brandNames)
+        {
+            brands.add(new Brand(++lastBrandId, name));
+        }
+    }
+
+    public Brand getBrandByName(String name)
+    {
+        for (Brand brand : brands)
+        {
+            if(brand.getBrandName().equalsIgnoreCase(name)) return brand;
+        }
+
+        return null;
+    }
+
+    public List<Brand> getBrands() {
+        Collections.sort(brands);
+        return brands;
+    }
+
+    public int getLastBrandId() {
+        return lastBrandId;
+    }
+
+    public void addNewBrand(String name){
+        brands.add(new Brand(++lastBrandId, name));
+    }
+
+    public void deleteBrand(Brand brand){
+        brands.remove(brand);
+    }
+
+    public Brand addAndGetBrand(String name){
+        Brand brand = new Brand(++lastBrandId, name);
+        brands.add(brand);
+        return brand;
+    }
+
+}
