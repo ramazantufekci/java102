1ddebfd750f83a5649d7718ce849226cad0931d0
Ramazan TÜFEKÇİ <ramazan.tufekci@outlook.com>
2023-10-03 15:04:10 +0300

    Create Address.java

---
+++ Insurance_Menagment_System/Address/Address.java
@@ -0,0 +1,27 @@
+package Address;
+
+public interface Address
+{
+    public String getCity() ;
+
+    public void setCity(String city) ;
+
+    public String getTown();
+
+    public void setTown(String town) ;
+
+    public String getNeighborhood() ;
+
+    public void setNeighborhood(String neighborhood) ;
+    public String getStreet() ;
+
+    public void setStreet(String street) ;
+
+    public String getBuilOrCompanyName() ;
+
+    public void setBuilOrCompanyName(String buildOrCompanyName) ;
+
+    public int getDoorOrComplexNumber() ;
+
+    public void setDoorOrComplexNumber(int buildOrComplexNumber) ;
+}
